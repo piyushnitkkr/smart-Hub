902a95ded37607fa061b1a9162bf9782d01c53e8
Piyush <145579268+piyushnitkkr@users.noreply.github.com>
2025-01-24 16:29:19 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://smart-hub-8hpd.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
