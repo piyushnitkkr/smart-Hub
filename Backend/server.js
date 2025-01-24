1c4ac8074933d1a43d72143a950260670b5b169f
Piyush <145579268+piyushnitkkr@users.noreply.github.com>
2025-01-24 21:03:23 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,8 +17,7 @@ const httpServer = app.listen(PORT, () => {
 // Dynamic CORS configuration
 const io = new Server(httpServer, {
   cors: {
-    "https://smart-hub-three.vercel.app",
-      "https://smart-hub-zrd3-7gp6j79pk-piyushnitkkrs-projects.vercel.app" // Use "*" temporarily for debugging
+    "https://smart-hub-three.vercel.app", // Use "*" temporarily for debugging
     methods: ["GET", "POST"],
   },
 });
