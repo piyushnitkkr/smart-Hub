c4e06cd385c6135a908a1c34a642e3af47a59e1f
Piyush <145579268+piyushnitkkr@users.noreply.github.com>
2025-01-25 12:43:23 +0530

    Update LiveChat.jsx

--- smartHub/src/components/LiveChat.jsx
+++ smartHub/src/components/LiveChat.jsx
@@ -14,7 +14,7 @@ function LiveChat() {
   const messagesEndRef = useRef(null)
 
   useEffect(() => {
-    const newSocket = io("https://smart-hub-three.vercel.app")
+    const newSocket = io("http://smart-hub-three.vercel.app")
     setSocket(newSocket)
 
     newSocket.on("initMessages", (fetchedMessages) => {
