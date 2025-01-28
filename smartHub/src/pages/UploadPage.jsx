d929926a4c2ec2d4cc0bbb49df044a443c11b4eb
Piyush <145579268+piyushnitkkr@users.noreply.github.com>
2025-01-28 19:16:44 +0530

    Update UploadPage.jsx

--- smartHub/src/pages/UploadPage.jsx
+++ smartHub/src/pages/UploadPage.jsx
@@ -29,7 +29,7 @@ function UploadPage() {
     try {
       setUploading(true)
 
-      const response = await fetch("https://smart-hub-three.vercel.app/upload", {
+      const response = await fetch("https://smart-hub-k3z0.onrender.com/upload", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
