2e1201210dc8effbd252a45982d81dcb92cffc91
Piyush <145579268+piyushnitkkr@users.noreply.github.com>
2025-01-29 17:02:22 +0530

    Update LogoHead.jsx

--- smartHub/src/components/LogoHead.jsx
+++ smartHub/src/components/LogoHead.jsx
@@ -7,7 +7,7 @@ function LogoHead() {
       <div className="container mx-auto px-4 py-6">
         <div className="flex flex-col items-center">
           <Link to="/" className="flex flex-col items-center gap-2">
-            <img src="/Smart.svg" alt="Logo" className="w-20 h-20 rounded-[50%]" />
+            <img src="/download.png" alt="Logo" className="w-20 h-20 rounded-[50%]" />
             <h1 className="text-3xl font-bold text-purple-400">Smart Study Hub</h1>
           </Link>
         </div>
